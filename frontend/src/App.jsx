006bdb44d5f78136ba969b8fee9d1ccc424eb0ce
yogitajoshi30 <joshiyogita67@gmail.com>
2025-08-25 18:30:17 +0530

    Routes Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import UserProvider from "./contexts/UserContext"
+import Dashboard from './pages/Dashboard'
+import EditResume from './components/EditResume'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <UserProvider>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/resume/:resumeId' element={<EditResume />} />
+      </Routes>
+
+      <Toaster toastOptions={{
+        className: "",
+        style: {
+          fontSize: "13px"
+        }
+      }}></Toaster>
+    </UserProvider>
   )
 }
 
