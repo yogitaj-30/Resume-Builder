bbffb6d1d88aecf864a2c998e85bcd706ae1e5a8
yogitajoshi30 <joshiyogita67@gmail.com>
2025-08-25 18:25:46 +0530

    Added Schema Validation for name, email and password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,19 +3,19 @@ const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        minlength: 3,
-        required: true
+        minlength: [3, "Name must be at least 3 characters long"],
+        required: [true, "Name is required"]
     },
     email: {
         type: String,
-        unique: true,
-        required: true,
+        unique: [true, "Email already exists"],
+        required: [true, "Email is required"],
         match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"]
     },
     password: {
         type: String,
-        min: 8,
-        required: true
+        minlength: 8,
+        required: [true, "Password is required"],
     },
 },
     {
